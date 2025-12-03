e84cac093a2aa477aaa2b8c512410c137e167857
alhassan <121292207+akanni-fijab@users.noreply.github.com>
2025-12-03 21:40:31 +0100

    Add files via upload

--- calculate.py
+++ calculate.py
@@ -1,18 +1,15 @@
-print("DO I STILL REMEMBER PYTHON")
-import func_imports
-
-
-x = int(input("Enter x val ")) # Inner most function is evaluated first
-y = int(input("Enter Y val ")) #Square brackets in documentation mean optional parameters
-
-
-# print(x / y)
-print(f"The square of {x} is {func_imports.square_input(x)}")
-print(f"The square of {y} is {func_imports.square_input(y)}")
-
-print(f"{x} raised to power {y} is {func_imports.pow(x,y)}")
-print(f"{y} raised to power {x} is {func_imports.pow(y,x)}")
-
-
-# print(f"{x/y:,.32f}")
+def main():
+    user_input = input("Enter some text ")
+    print(convert(user_input))
+    
+def  convert(some_str)->str:
   
+    some_str =some_str.replace(":)", "🙂") # It replaces all occurences not just the first
+    some_str =some_str.replace(":(", "🙁") 
+
+    return some_str
+        
+
+    
+
+main()
