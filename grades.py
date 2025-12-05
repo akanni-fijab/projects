319d2983fd70d362ae6b2aa6205b5f54872eb18f
alhassan <121292207+akanni-fijab@users.noreply.github.com>
2025-12-05 18:57:41 +0100

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,29 @@
+print("Grades Calculator 😃")
+print("Hey Mom and Dad 👽")
+
+
+student_grade = int(input("Enter Your grade : "))
+
+if student_grade >= 90 :
+    print("Grade A")
+elif student_grade >= 80 : # Remember the process of evaluating an elif block {{{{{{}}}}}}
+    print("Grade B")
+elif student_grade >= 70:
+    print("Grade C")
+elif student_grade >= 60:
+    print("Grade D")
+
+
+
+
+
+# if 90 <=student_grade <=100: # just like on pen and paper
+#     print("Grade A ❤️‍🔥")
+# elif 80 <=student_grade <=  89 :
+#     print("Grade B 🤜🏾")
+# elif 70 <=student_grade <= 79:
+#     print("Grade C 🧓")
+# elif 60 <= student_grade <=69:
+#     print("Grade D 💂🏿‍♂️")
+# else:
+#     print("Grade is strangely F 🎇")
