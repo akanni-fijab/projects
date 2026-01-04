0b5a7fa9ec70b00650f278e33c472487ade33763
alhassan <121292207+akanni-fijab@users.noreply.github.com>
2026-01-04 07:06:42 +0100

    Querying art institue of chicagoo

---
+++ api.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def main():
+    page = 1
+    limit = 100
+    querry = input("Enter an artist: ")
+
+    url = f"https://api.artic.edu/api/v1/artworks/search?page={page}&limit={limit}"
+    # header = {"User-Agent": "Mozzila/5.0"}
+    try:
+        req = requests.get(url, querry)
+        req.raise_for_status()
+        obj = req.json()
+
+    except requests.HTTPError:
+        print("Connection issue")
+        return  # it is a function so it exits on errors "return"
+
+    for artwork in obj["data"]:
+        print(f"* {artwork['title']}")
+
+
+main()
