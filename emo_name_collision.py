0fd1defaf42bdf16099d2eeed6f76cea7dd4e485
alhassan <121292207+akanni-fijab@users.noreply.github.com>
2026-01-13 08:38:11 +0100

    Add files via upload

---
+++ emo_name_collision.py
@@ -0,0 +1,9 @@
+import emoji
+
+in_emoji = input("Input: ")
+
+
+print(f"Output: {emoji.emojize(in_emoji, language='alias')}")
+# print(emoji.emojize('Python is :thumbs_up:'))
+# print(emoji.emojize("Python is :thumbs_up:"))
+# emoji.analyze
