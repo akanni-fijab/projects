838cb2bf569422e950dd80aa97d6e91a6bcbc0b5
alhassan <121292207+akanni-fijab@users.noreply.github.com>
2026-01-17 06:30:28 +0100

    Add files via upload

--- figlet_figs.py
+++ figlet_figs.py
@@ -16,14 +16,14 @@ if len(sys.argv) == 1:  # no input
     u_string = input("Input: ")  # Pick random font with input
 
     figlet.setFont(font=random.choice(my_obj))
-    print(f"{figlet.renderText(u_string)}")
+    print(f"Output: \n{figlet.renderText(u_string)}")
 
 elif sys.argv[1] in font_choose:  # entered a flag so some input required
-    fig_font = sys.argv[2]
+    fig_font = sys.argv[2]  # errors out if -f no font specified
     figlet.setFont(font=sys.argv[2])
 
     u_string = input("Input: ")
-    print(figlet.renderText(u_string))
+    print(f"Output: \n{figlet.renderText(u_string)}")
 
 elif (
     sys.argv[1] not in my_obj or sys.argv[1] is None
