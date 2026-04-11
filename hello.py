7936316ca4f34211d2e695c7aeeb4128b9bf68e6
alhassan <121292207+akanni-fijab@users.noreply.github.com>
2026-04-11 16:43:56 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+# Name: Al Hassan Oluwatobi Fijabi
+# Date: 31/03/2026
+#
+def main():
+    name = input("What is your name: ")
+    print(hello(name))
+
+
+def hello(to="World"):
+    return f"Hello {to}"
+
+
+if __name__ == "__main__":
+    main()
