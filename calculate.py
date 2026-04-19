1e59f9d8aa8464df90a1f1c9ace9b735b198c96e
Al Hassan Fijabi <242030010@nileuniversity.edu.ng>
2026-04-19 16:43:29 +0100

    working ssh key

--- calculate.py
+++ calculate.py
@@ -1,15 +1,19 @@
 def main():
     user_input = input("Enter some text ")
     print(convert(user_input))
-    
-def  convert(some_str)->str:
-  
-    some_str =some_str.replace(":)", "🙂") # It replaces all occurences not just the first
-    some_str =some_str.replace(":(", "🙁") 
+
+
+def convert(some_str) -> str:
+
+    some_str = some_str.replace(
+        ":)", "🙂"
+    )  # It replaces all occurences not just the first
+    some_str = some_str.replace(":(", "🙁")
 
     return some_str
-        
 
-    
+
+# it is working oooooooooooooooooooooooooooooooooooooooo
+
 
 main()
