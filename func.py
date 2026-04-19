17381c2992d0718ad1bcbc9f42826305fab128db
Al Hassan Fijabi <242030010@nileuniversity.edu.ng>
2026-04-19 16:50:11 +0100

    fileio initial commit

--- func.py
+++
@@ -1,6 +0,0 @@
-import func_imports
-
-name = input("Enter your name: ").capitalize().title()
-
-func_imports.greet(user_name=name)
-
--- say.py
+++
@@ -1,8 +0,0 @@
-import sys
-from say_lib import hello
-
-
-if len(sys.argv) == 2:
-    hello(sys.argv[1])
-
-# main()
